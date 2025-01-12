d5efc3305e117345c285a895949673e070d8bb18
Aabrity <aabrityd@gmail.com>
2025-01-12 16:31:19 +0545

    Complete profile funtionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
-import { FaSearch } from 'react-icons/fa';
-import { Link, useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
+import { FaSearch } from 'react-icons/fa';
+import { useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
@@ -27,18 +27,18 @@ export default function Header() {
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>Sahand</span>
-            <span className='text-slate-700'>Estate</span>
+            <span className='text-slate-500'>OPEN</span>
+            <span className='text-slate-700'>NEST</span>
           </h1>
         </Link>
         <form
           onSubmit={handleSubmit}
-          className='bg-slate-100 p-3 rounded-lg flex items-center'
+          className='bg-slate-100 p-3 rounded-full flex items-center'
         >
           <input
             type='text'
             placeholder='Search...'
-            className='bg-transparent focus:outline-none w-24 sm:w-64'
+            className='bg-transparent focus:outline-none w-24 sm:w-80'
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
           />
